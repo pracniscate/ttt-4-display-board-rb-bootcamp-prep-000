a9c55c8155c2b46c0232cd4f7fa142bec3f93396
Lorena <lorena.giddens@outlook.com>
2018-04-21 05:17:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+<<<<<<< HEAD
 =begin
 def display_board(board) 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -19,3 +20,27 @@ def display_board(board)
          [" #{board[6]} | #{board[7]} | #{board[8]} "]
   puts rows
 end
+=======
+# prints a blank board when the board array is empty
+
+def display_board(board) 
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end 
+
+# prints a blank board when the board array is empty
+
+# prints an entire board full of Xs
+# prints an entire board full of Os
+
+def display_board(board) 
+  puts " #{board[0]} | #{board[1]} | #{board[3]} "
+  puts "-----------"
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
+  puts "-----------"
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
+end 
+>>>>>>> 05b8a71cd060d2fbf6ee243feaa5d872626d26f1
